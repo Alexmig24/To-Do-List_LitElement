05e59915ecca81ae37e66e57e6da1b8eea0c1f55
Alexmig24 <alexandermqc2004@hotmail.com>
2025-07-03 12:55:48 -0500

    Update de la ruta para el index.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin=require('html-webpack-plugin');
 module.exports={
     mode: 'development',
     devtool: 'source-map',
-    entry: './task-manager/index.js',
+    entry: './task-manager/src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js'
